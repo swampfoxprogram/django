207c50fc62ff96cb7cb6ebcbf50fa3531cd9fbdc
prasad <73817848+swampfoxprogram@users.noreply.github.com>
2023-03-16 00:31:35 +0530

    Update urls.py

--- urls.py
+++ urls.py
@@ -6,7 +6,7 @@ from django.contrib import admin
 from django.urls import path
 from django.conf.urls.static import static
 from django.conf import  settings 
-from . import views
+from import views
 
 
 urlpatterns = [
